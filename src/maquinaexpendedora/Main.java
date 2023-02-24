5d7a8d99715e083e464494dd658499d81de860d8
Javier Benítez <Javier Bentez@Javier>
2023-02-24 20:04:19 +0100

    Proyecto BenitezGarciaJavier2223 comentado con JavaDoc

--- src/maquinaexpendedora/Main.java
+++ src/maquinaexpendedora/Main.java
@@ -2,20 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package máquinaexpendedora;
+package maquinaexpendedora;
 
 /**
- *
- * @author efech
+ * Clase main del programa, que define un objeto "Máquina Expendedora" con unos parámetros y realiza
+ * acciones de subir el precio y de sacar snacks de ella.
+ * 
+ * @author Javier Benítez García
  */
 public class Main {
 
     /**
-     * @param args the command line arguments
+     * Método main.
+     * Es el método principal de ejecución del programa
+     * 
+     * @param args los argumentos de la línea de comandos
      */
     public static void main(String[] args) {
         // TODO code application logic here
-              MaquinaExpendedoraBenitezGarciaJavier2223 snack;
+        MaquinaExpendedoraBenitezGarciaJavier2223 snack;
        
         
         snack=new MaquinaExpendedoraBenitezGarciaJavier2223(13,2,5);
@@ -31,10 +36,16 @@ public class Main {
         
     }
 
+    /** Método  obtener3_UnidadesBenitezGarciaJavier2223.
+    * 
+    * Vamos a intentar sacar 12 snack, y tengo 50 euros.
+    * Como no hay unidades suficientes en la máquina saltará
+    * una excepción y no se modificará el nº de unidades de la tienda
+    * 
+    * @param snack hace referencia a la clase MaquinaExpendedoraBenitezGarciaJavier2223
+    * @throws Exception si no hay suficientes unidades para obtener
+    */
     private static void obtener3_UnidadesBenitezGarciaJavier2223(MaquinaExpendedoraBenitezGarciaJavier2223 snack) {
-        /*Vamos a intentar sacar 12 snack, y tengo 50 euros.
-        Como no hay unidades suficientes en la máquina saltará
-        una excepción y no se modificará el nº de unidades de la tienda*/
         try {
             System.out.println("3º.- Vamos a sacar snacks (queremos 12 y tenemos 50 €)");
             snack.sacarsnack(12, 50, pegatina);
@@ -46,10 +57,16 @@ public class Main {
         }
     }
 
+    /** Método  obtener2_UnidadesBenitezGarciaJavier2223.
+    * 
+    * Vamos a intentar sacar 2 snack, y tengo 9 euros.
+    * Ahora se actualizará el número de snack que tenemos en
+    * la máquina
+    * 
+    * @param snack hace referencia a la clase MaquinaExpendedoraBenitezGarciaJavier2223
+    * @throws Exception si no hay suficiente dinero
+    */
     private static void obtener2_UnidadesBenitezGarcia2223(MaquinaExpendedoraBenitezGarciaJavier2223 snack) {
-        /*Vamos a intentar sacar 2 snack, y tengo 9 euros.
-        Ahora se actualizará el número de snack que tenemos en
-        la máquina*/
         try {
             System.out.println("2º.- Vamos a sacar snacks (queremos 2 y tenemos 9 €)");
             snack.sacarsnack(2, 9, pegatina);
@@ -61,10 +78,16 @@ public class Main {
         }
     }
 
+    /** Método  obtener1_UnidadesBenitezGarciaJavier2223.
+    * 
+    * Vamos a intentar sacar 2 snack, y tengo 5 euros.
+    * Como no hay dinero suficiente saltará
+    * una excepción y no se modificará el nº de unidades de la tienda
+    * 
+    * @param snack hace referencia a la clase MaquinaExpendedoraBenitezGarciaJavier2223
+    * @throws Exception si no hay suficiente dinero
+    */
     private static void obtener1_UnidadesBenitezGarciaJavier2223(MaquinaExpendedoraBenitezGarciaJavier2223 snack) {
-        /*Vamos a intentar sacar 2 snack, y tengo 5 euros.
-        Como no hay dinero suficiente saltará
-        una excepción y no se modificará el nº de unidades de la tienda*/
         try {
             System.out.println("1º.- Vamos a sacar snacks (queremos 2 y tenemos 5 €)");
             snack.sacarsnack(2, 5, pegatina);
@@ -76,9 +99,15 @@ public class Main {
         }
     }
 
+    /** Método  sube2_PrecioBenitezGarciaJavier2223.
+    * 
+    * Vamos a aumentar el precio 3€, como supera el precio máximo establecido la operación
+    * no tendrá éxito y se mantendrá el precio anterior
+    * 
+    * @param snack hace referencia a la clase MaquinaExpendedoraBenitezGarciaJavier2223
+    * @throws Exception si se supera el precio máximo de 5€
+    */
     private static void sube2_PrecioBenitezGarciaJavier2223(MaquinaExpendedoraBenitezGarciaJavier2223 snack) {
-        /*Vamos a aumentar el precio 3€, como supera el precio máximo establecido la operación
-        no tendrá éxito y se mantendrá el precio anterior*/
         try {
             System.out.println("Vamos a aumentar el precio de venta de un snack por encima del precio máximo");
             snack.aumentarPrecio(3);
@@ -88,9 +117,15 @@ public class Main {
         }
     }
 
+    /** Método  sube1_PrecioBenitezGarciaJavier2223.
+    * 
+    * Vamos a aumentar el precio 1€, como no supera el precio máximo establecido la operación
+    * tendrá éxito y el nuevo precio será de 3€
+    * 
+    * @param snack hace referencia a la clase MaquinaExpendedoraBenitezGarciaJavier2223
+    * @throws Exception si ocurre un error al aumentar el precio
+    */
     private static void sube1_PrecioBenitezGarciaJavier2223(MaquinaExpendedoraBenitezGarciaJavier2223 snack) {
-        /*Vamos a aumentar el precio 1€, como no supera el precio máximo establecido la operación
-        tendrá éxito y el nuevo precio será de 3€*/
         try {
             System.out.println("Vamos a aumentar el precio de venta de un snack por debajos del precio máximo");
             snack.aumentarPrecio(1);
--- src/maquinaexpendedora/MaquinaExpendedoraBenitezGarciaJavier2223.java
+++ src/maquinaexpendedora/MaquinaExpendedoraBenitezGarciaJavier2223.java
@@ -2,48 +2,82 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package máquinaexpendedora;
+package maquinaexpendedora;
 
 /**
+ * Clase MaquinaExpendedoraBenitezGarciaJavier2223, que tiene los métodos usados por la clase Main
  *
- * @author efech
+ * @author Javier Benítez García
  */
 
+/**
+* Método MaquinaExpendedoraBenitezGarciaJavier2223.
+* Es el método principal de la clase
+*/
 public class MaquinaExpendedoraBenitezGarciaJavier2223 {// Consideramos máquina snack precio único
     private int unidades;//número de snacks disponbles en la máquina expendora 
     private double precio;//precio actual de un snack
     private double precioMaximoBenitezGarciaJavier2223;//precio máximo que puede tener un snack
 
-    /*Constructor por defecto*/
+    /**
+     * Constructor por defecto
+     */
     public MaquinaExpendedoraBenitezGarciaJavier2223() {
     
     }
     
     
-     /*Constructor por parámetros*/
+     /**
+      * Constructor por parámetros del método principal de esta clase
+      * 
+      * @param unidades numero de snacks disponibles en la máquina
+      * @param precio precio actual de un snack
+      * @param precio_max precio máximo que puede tener un snack
+      */
     public MaquinaExpendedoraBenitezGarciaJavier2223(int unidades, double precio, double precio_max) {
         this.unidades = unidades;
         this.precio = precio;
         this.precioMaximoBenitezGarciaJavier2223 = precio_max;
     }
     
-    /*Método que devuelve el número de snack que tiene la máquina*/
+    /**
+     * Método obtenerUnidades.
+     * 
+     * Método que devuelve el número de snack que tiene la máquina
+     */
     public int obtenerUnidades() {
         return this.unidades;
     }
 
-    /*Método que devuelve el precio que tiene cada snack*/
+    /**
+     * Método obtenerPrecio.
+     * 
+     * Método que devuelve el precio que tiene cada snack
+     */
     public double obtenerPrecio() {
         return this.getPrecio();
     }
     
-     /*Método que permite modificar el número de snack dsiponbles de la máquina*/
+     /**
+      * Método modificarUnidades.
+      * 
+      * Método que permite modificar el número de snack dsiponbles de la máquina
+      * 
+      * @param unidades número de unidades a modificar
+      */
     public void modificarUnidades(int unidades) {
         this.unidades = unidades;
     }
     
-     /*Método que permite aumentar el precio de venta, suma al precio actual 
-    el aumento que se indica siempre que no se sobrepase el precio máximo de venta*/
+     /**
+      * Método aumentarPrecio.
+      * 
+      * Método que permite aumentar el precio de venta, suma al precio actual 
+      * el aumento que se indica siempre que no se sobrepase el precio máximo de venta
+      * 
+      * @param aumento cuantos euros debe aumentar el precio
+      * @throws Exception el aumento debe ser positivo o no se puede superar el precio máximo
+      */
     public void aumentarPrecio(double aumento) throws Exception {
         if (aumento <= 0) {
             throw new Exception("El aumento debe ser positivo");
@@ -56,8 +90,18 @@ public class MaquinaExpendedoraBenitezGarciaJavier2223 {// Consideramos máquina
     
        
     
-    /*Método que permite obtener un snack si se tiene suficiente dinero y hay 
-suficientes unidades en la máquina*/
+    /**
+     * Método sacarsnack.
+     * 
+     * Método que permite obtener un snack si se tiene suficiente dinero y hay 
+     * suficientes unidades en la máquina
+     *
+     * @param unidades cuantos snacks se quiere sacar de la máquina expendedora
+     * @param dinero cuanto dinero hay disponible
+     * @param regaloBenitezGarciaJavier2223 por defecto tiene el valor "pegatina"
+     * @throws Exception No puede introducirse un valor negativo para el dinero, no tiene suficiente dinero
+     * la cantidad de snacks no puede ser negativa o no hay suficientes snack en la máquina
+     */
     public void sacarsnack(int unidades, double dinero, String regaloBenitezGarciaJavier2223) throws Exception {
         if (dinero <= 0) {
             throw new Exception("No puede introducirse un valor negativo para el dinero");
@@ -75,6 +119,10 @@ suficientes unidades en la máquina*/
     }
 
     /**
+     * Método getPrecio.
+     * 
+     * Devuelve el precio actual de los snacks
+     * 
      * @return the precio
      */
     public double getPrecio() {
@@ -82,13 +130,21 @@ suficientes unidades en la máquina*/
     }
 
     /**
-     * @param precio the precio to set
+     * Método setPrecio.
+     * 
+     * Establece un nuevo precio a los snacks
+     * 
+     * @param precio nuevo precio a establecer
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
     /**
+     * Método getPrecioMaximoBenitezGarciaJavier2223.
+     * 
+     * Devuelve el precio máximo actual
+     * 
      * @return the precioMaximoBenitezGarciaJavier2223
      */
     public double getPrecioMaximoBenitezGarciaJavier2223() {
@@ -96,7 +152,11 @@ suficientes unidades en la máquina*/
     }
 
     /**
-     * @param precioMaximoBenitezGarciaJavier2223 the precioMaximoBenitezGarciaJavier2223 to set
+     * Método setPrecioMaximoBenitezGarciaJavier2223.
+     * 
+     * Establece un nuevo precio máximo a los snacks
+     * 
+     * @param precioMaximoBenitezGarciaJavier2223 nuevo precio máximo a establecer
      */
     public void setPrecioMaximoBenitezGarciaJavier2223(double precioMaximoBenitezGarciaJavier2223) {
         this.precioMaximoBenitezGarciaJavier2223 = precioMaximoBenitezGarciaJavier2223;
